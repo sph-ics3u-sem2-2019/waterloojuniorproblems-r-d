fd9b213df34ba0f6d6c9e59fa120bc58a80a0dc2
daniadeleon <47532261+daniadeleon@users.noreply.github.com>
2019-03-05 09:54:03 -0500

    Complete
    Done the telemarketer problem.

--- src/Telemarketer.java
+++ src/Telemarketer.java
@@ -6,35 +6,50 @@ public class Telemarketer {
 		/*
 		 * Follow the instructions in JuniorProblem.pdf
 		 * Use the following values to test your program and record your results
-		 * 1) 8,6,6,7
-		 * 2) 7,6,6,8
-		 * 3) 9,5,6,9
-		 * 4) 3,4,5,6
-		 * 5) 0,8,8,0
-		 * 6) 8,5,5,9
-		 * 7) 9,1,1,9
-		 * 8) 9,9,9,9
-		 * 9) 9,1,2,8
-		 * 10) 8,7,4,8 
-		 * 11) 8,2,2,7
-		 * 12) 1,3,3,9
-		 * 13) 1,0,2,6
+		 * 1) 8,6,6,7 Answer
+		 * 2) 7,6,6,8 Answer
+		 * 3) 9,5,6,9 Answer
+		 * 4) 3,4,5,6 Answer
+		 * 5) 0,8,8,0 Answer
+		 * 6) 8,5,5,9 Ignore
+		 * 7) 9,1,1,9 Ignore
+		 * 8) 9,9,9,9 Ignore
+		 * 9) 9,1,2,8 Answer
+		 * 10) 8,7,4,8 Answer
+		 * 11) 8,2,2,7 Answer
+		 * 12) 1,3,3,9 Answer
+		 * 13) 1,0,2,6 Answer
 		 */
+		int num1=0;
+		int num2=0;
+		int num3=0;
+		int num4=0;
 		Scanner in=new Scanner(System.in);
-		int num1=in.nextInt();
-		int num2=in.nextInt();
-		int num3=in.nextInt(); 
-		int num4=in.nextInt();
+		System.out.println("num1:");
+		num1=in.nextInt();
+		System.out.println("num2:");
+		num2=in.nextInt();
+		System.out.println("num3:");
+		num3=in.nextInt();
+		System.out.println("num4:");
+		num4=in.nextInt();
+		System.out.println("Phone call");
 		if (num1==8||num1==9) { 
-			if (num4==8||num4==9);
-			if (num2==num3);
+			if (num2==num3) {
+				if (num4==8||num4==9) {
+					System.out.println("Ignore");
+				}else {
+					System.out.println("Answer");
+				}
+			}else {
+				System.out.println("Answer");
+			}
 		} else {
-		return;
+			System.out.println("Answer");
 		}
-		
-		
-		
-		
-		
+
+
+
+
 	}
 }
